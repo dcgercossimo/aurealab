faa31f97435fb96cfdf82a682b0e524919c32114
DCGercossimo <dcgercossimo@gmail.com>
2025-04-18 00:00:36 -0300

    feat: add `pepper` in .env and use in model `password`

--- models/password.js
+++ models/password.js
@@ -2,15 +2,23 @@ import bcryptjs from 'bcryptjs';
 
 async function hash(password) {
   const rounds = getNumberOfRounds();
-  return await bcryptjs.hash(password, rounds);
+  const pepper = getPepper();
+  const passwordWithPepper = `${password}${pepper}`;
+  return await bcryptjs.hash(passwordWithPepper, rounds);
 }
 
 function getNumberOfRounds() {
   return process.env.NODE_ENV === 'production' ? 14 : 1;
 }
 
+function getPepper() {
+  return process.env.PEPPER || 'pimentinha';
+}
+
 async function compare(providedPassword, storePassword) {
-  return await bcryptjs.compare(providedPassword, storePassword);
+  const pepper = getPepper();
+  const passwordWithPepper = `${providedPassword}${pepper}`;
+  return await bcryptjs.compare(passwordWithPepper, storePassword);
 }
 
 const password = {
